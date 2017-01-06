abb0b02d8513efdfa979bd5687596a7140a707bd
Sandeep Bedadala <sandeepbedadala@gmail.com>
2017-01-06 15:39:21 -0500

    Working on mouse hovers

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+//
+//   $("#toggle").hide();
+//   // $("#06").hide();
+//   // $(".06").show();
+//   // $("#05").hide();
+//   // $(".05").show();
+//
+//   $(img).hover(function(){
+//       $(this).$("#toggle").slideToggle();
+//       $(this).$("#06").slideToggle();
+//       $(this).$("#05").slideToggle();
+//   });
+//
+//   // $(".05").hover(function(){
+//   //     $("#toggle").slideToggle();
+//   //     $("#05").slideToggle();
+//   // });
+// });
+
+
+// $(document).ready(function(){
+//   $("#toggle").hover(function(){
+//     document.getElementById(toggle).style.opacity = "0.5";
+//     console.log('its coming here');
+//   });
+// });
