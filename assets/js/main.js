d5b99b6aff6096f4f893c9090507e932654a1d1b
Sandeep Bedadala <sandeepbedadala@gmail.com>
2017-01-13 12:18:11 -0500

    made image responsive

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// jQuery.noConflict();
+// jQuery(document).ready(function($){
+//   $('#byjus01 img').each(function(){
+//   $(this).removeAttr('width')
+//   $(this).removeAttr('height');
+//   console.log('hellooooo');
+//  });
+// });
